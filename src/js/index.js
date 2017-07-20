a1faeae875d2fe18dc4d2101a412410df45d121c
Mr.Yuan <ysdn20z@163.com>
2017-07-20 12:08:17 +0800

    'change'

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 /**
  * Created by Mr.Yuan on 2017/7/19 0019.
  */
+import '../sass/common.scss'
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,15 +1,16 @@
 /**
  * Created by yanshan on 2017/7/19.
  */
-let webpack = require('webpack'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    OpenBrowserPlugin = require('open-browser-webpack-plugin');
+let webpack = require('webpack');
+let OpenBrowserPlugin = require('open-browser-webpack-plugin');
+let HtmlPlugin = require('html-webpack-plugin');
+let FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = {
     context: __dirname + "/src",
     entry: {
         index: './js/index.js',
-        vendor: ['properScreen.js','./js/lib/properScreen_css.js']
+        vendor: ['./js/lib/properScreen.js','./js/lib/properScreen_css.js']
     },
     output: {
         filename: "js/[name].bundle.js"
@@ -18,14 +19,71 @@ module.exports = {
         contentBase: __dirname + '/src',
         port: 3000
     },
+    module: {
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: "html-loader?-minimize",
+                options: {
+                    attrs:'img:src img:data-src'
+                }
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader','css-loader']
+            },
+            {
+                test: /\.less$/,
+                loaders:  ['style-loader','css-loader','less-loader']
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader','css-loader','sass-loader']
+            },
+            {
+                test:/\.(png|jpg)$/,
+                loader: 'url-loader',
+                options: { limit: 1048576 }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: { limit: 1048576 }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'jquery': __dirname +'/src/js/lib/jQuery1.11.3.min.js'
+        }
+    },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
             name: "vendor",
             filename: "js/commons.js",
             minChunks: 3,
         }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jquery: 'jquery'
+        }),
         new OpenBrowserPlugin({
             url: 'http://localhost:3000/views/index.html'
-        })
+        }),
+
+        new HtmlPlugin({
+            filename: 'views/index.html',
+            template: 'views/index.html',
+            chunks: ['index','vendor']
+        }),
+        new FriendlyErrorsPlugin()
     ]
 };
